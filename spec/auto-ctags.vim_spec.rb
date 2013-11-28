1e64e47f9fd7be1c8f742e83cd62151886d06cfd
soramugi <soramugi.chika@gmail.com>
2013-11-29 03:11:45 +0900

    Argument expansion of command

--- spec/auto-ctags.vim_spec.rb
+++ spec/auto-ctags.vim_spec.rb
@@ -1,21 +1,20 @@
 require 'spec_helper'
 
 describe 'auto-ctags.vim' do
-  def repo_root()
+  def repo_root
     File.expand_path('../..', __FILE__)
   end
 
-  def runcommand(command)
-    VIM.command("command")
+  def run(command)
+    VIM.command("#{command}")
   end
 
   describe ':Ctags' do
-    after(:each) do
-    end
     it 'create the tags' do
-      runcommand(':Ctags')
-      FileTest.exist?(repo_root + '/tags').should be_true
-      File.delete(repo_root + '/tags')
+      run("Ctags")
+      path = repo_root + '/tags'
+      File.exist?(path).should be_true
+      File.delete(path)
     end
   end
 end
