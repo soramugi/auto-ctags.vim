a89ce1fa0be7c59f0b1172f23ad873cedac11744
soramugi <soramugi.chika@gmail.com>
2013-11-29 02:01:14 +0900

    :Ctags test

--- spec/auto-ctags.vim_spec.rb
+++ spec/auto-ctags.vim_spec.rb
@@ -1,17 +1,21 @@
-require 'vimrunner'
-
-vim = Vimrunner.start
-vim.add_plugin(File.expand_path('../..', __FILE__), 'plugin/auto-ctags.vim')
+require 'spec_helper'
 
 describe 'auto-ctags.vim' do
-  after(:all) do
-    vim.kill
+  def repo_root()
+    File.expand_path('../..', __FILE__)
+  end
+
+  def runcommand(command)
+    VIM.command("command")
   end
 
   describe ':Ctags' do
+    after(:each) do
+    end
     it 'create the tags' do
-      vim.command(':Ctags')
-      File.file?('tags').should
+      runcommand(':Ctags')
+      FileTest.exist?(repo_root + '/tags').should be_true
+      File.delete(repo_root + '/tags')
     end
   end
 end
